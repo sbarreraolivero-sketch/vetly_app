2c830f7ed9d40b28fd95d80860a8c17a0c158924
Sebastian Barrera olivero <sebabarrera@MacBook-Air-de-Sebastian.local>
2026-04-23 14:04:21 -0400

    fix: removed unused imports to fix vercel build

--- src/pages/KnowledgeBase.tsx
+++ src/pages/KnowledgeBase.tsx
@@ -21,8 +21,6 @@ import {
     Lightbulb,
     Check,
     Info,
-    DollarSign,
-    Globe,
 } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { useAuth } from '@/contexts/AuthContext'
